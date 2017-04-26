f8c9a991dc6004d5ecc235e378fe532df6c4a349
Kyle McKean <mckean.kylej@gmail.com>
2017-04-26 17:01:51 -0400

    requests

---
+++ trasa-reflex/default.nix
@@ -0,0 +1,5 @@
+{ test ? "true" }:
+let parseBool = str: with builtins;
+  let json = fromJSON str; in if isBool json then json else throw "nix parseBool: ${str} is not a bool.";
+in
+import ../nix/default.nix { package = "trasa-reflex"; frontend = false; test = parseBool test; }
--- trasa-reflex/src/Data/Trasa/Reflex.hs
+++ trasa-reflex/src/Data/Trasa/Reflex.hs
@@ -1,2 +1,34 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Data.Trasa.Reflex where
 
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import qualified Data.Binary.Builder as B (toLazyByteString)
+import qualified Data.ByteString.Lazy as LBS (toStrict)
+
+import Network.HTTP.Types.URI
+import Reflex.Dom
+
+import Trasa.Core
+
+request :: forall t m rt rp.
+  MonadWidget t m
+  => (forall cs' rq' rp'. rt cs' rq' rp' -> T.Text)
+  -> (forall cs' rq' rp'. rt cs' rq' rp' -> Path CaptureEncoding cs')
+  -> (forall cs' rq' rp'. rt cs' rq' rp' -> RequestBody (Many BodyEncoding) rq')
+  -> (forall cs' rq' rp'. rt cs' rq' rp' -> ResponseBody (Many BodyDecoding) rp')
+  -> Event t (Prepared rt rp)
+  -> m (Event t (Either T.Text rp))
+request toMethod toCapEncs toReqBody toRespBody prepared =
+  fmap parseXhrResponse <$> performRequestAsync (xhrRequest <$> prepared)
+  where parseXhrResponse :: XhrResponse -> Either T.Text rp
+        parseXhrResponse = undefined
+        xhrRequest :: Prepared rt rp -> XhrRequest ()
+        xhrRequest p@(Prepared route captures reqBody) =
+          XhrRequest (toMethod route) (url p) conf
+          where url :: Prepared rt rp -> T.Text
+                -- This use of decodeUtf8 is safe because http-types has a postconditon that the builder is utf8 encoded
+                url = TE.decodeUtf8 . LBS.toStrict . B.toLazyByteString . encodePathSegments . linkWith toCapEncs
+                conf :: XhrRequestConfig ()
+                conf = def & xhrRequestConfig_sendData .~ ()
