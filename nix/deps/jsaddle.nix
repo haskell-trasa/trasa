bba75bfc21f12481c49cd394443e0d774c0d78ff
chessai <chessai1996@gmail.com>
2018-01-17 09:59:47 -0500

    fix jsaddle-deps

--- nix/deps/jsaddle.nix
+++ nix/deps/jsaddle.nix
@@ -8,9 +8,9 @@ mkDerivation {
   pname = "jsaddle";
   version = "0.9.4.0";
   src = fetchgit {
-    url = "https://github.com/chessai/jsaddle.git";
+    url = "https://github.com/ghcjs/jsaddle";
     sha256 = "11gjqqh859j8n9ixzbwqwl692ysank408qzw0dijz6nlv3b0x86f";
-    rev = "233b9d588c715c1a4f070713de2f89ecb9b2cd4a";
+    rev = "3f8b32833917f1a2dfbdb81ef00992fb54733c9a";
   };
   postUnpack = "sourceRoot+=/jsaddle; echo source root reset to $sourceRoot";
   libraryHaskellDepends = [
