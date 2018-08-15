a291b92edd09db66430866346d9a26ffdc6185f8
chessai <chessai1996@gmail.com>
2018-08-15 17:25:21 -0400

    update nix to work with nix 2.04

--- nix/default.nix
+++ nix/default.nix
@@ -1,11 +1,13 @@
 { package ? "trasa", test ? true, test-all ? false, frontend ? false }:
 let
-  fetchNixpkgs = import ./fetchNixpkgs.nix;
-  nixpkgs = fetchNixpkgs {
-    rev = "cda0f9fedef64f08fdd97ca81d48a5aaea047006"; 
-    sha256 = "161nz05x6ifkdbq947v82jh1qqv0wf1qlbsgl2wj5zydvas2cyy8";
+  _nixpkgs = import <nixpkgs> {}; 
+  nixpkgs = _nixpkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "d2b08c432cc9ec35691ab27085dfc7c6f404448b";
+    sha256 = "1s1kbyszlw8270f9brvvxfchx03ylawssmg0sqbx2msly4fzr01v";
   };
-  pkgs = import nixpkgs { config = {}; };
+  pkgs = import nixpkgs { config = {}; overlays = []; };
   inherit (pkgs) haskell;
 
   fetch-github-json = owner: repo: path:
@@ -16,12 +18,9 @@ let
         inherit (commit) rev sha256;
     };
 
-    #reflex-platform = import (fetch-github-json "reflex-frp" "reflex-platform" ./reflex-platform.json) {};
-    reflex-platform = import (fetch-github-json "chessai" "reflex-platform" ./chessai-reflex-platform.json) {};
+    reflex-platform = import (fetch-github-json "reflex-frp" "reflex-platform" ./reflex-platform.json) {};
 
-    compiler = if frontend then "ghcjs" else "ghc8_2_1";
-
-    filterPredicate = p: type:
+    compiler = if frontend then "ghcjs" else "ghc8_2_1"; filterPredicate = p: type:
       let path = baseNameOf p; in !(
            (type == "directory" && path == "dist")
         || (type == "directory" && path == "dist-newstyle")
