6db8abccb23f1383bbe1027707b625842e21436d
chessai <chessai1996@gmail.com>
2019-03-23 16:02:11 -0400

    make common module build

--- example/common/src/Common.hs
+++ example/common/src/Common.hs
@@ -1,20 +1,20 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE KindSignatures #-}
-{-# OPTIONS_GHC -Wall -Werror -Wno-unticked-promoted-constructors #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Common where
 
-import Data.Kind (Type)
-
+import Data.List.NonEmpty (NonEmpty(..))
 import Data.Bifunctor (first)
-import qualified Data.Text as T
-
-import Data.Vinyl (Rec)
+import Data.Kind (Type)
+import Topaz.Rec (Rec)
 import Trasa.Core
+import qualified Data.Text as T
 import qualified Trasa.Method as M
+
 newtype Key = Key Int deriving (Enum, Eq, Ord)
 
 instance Show Key where
@@ -40,21 +40,13 @@ data Route :: [Type] -> [Param] -> Bodiedness -> Type -> Type where
   ViewR :: Route '[Key] '[] Bodyless Person
   ViewAllR :: Route '[] '[Optional Int] Bodyless [Keyed Person]
 
-data Meta ps qs rq rp = Meta
-  { metaPath :: Path CaptureCodec ps
-  , metaQuery :: Rec (Query CaptureCodec) qs
-  , metaRequestBody :: RequestBody BodyCodec rq
-  , metaResponseBody :: ResponseBody BodyCodec rp
-  , metaMethod :: Method
-  }
-
-meta :: Route ps qs rq rp -> Meta ps qs rq rp
+meta :: Route caps qrys req resp -> MetaCodec caps qrys req resp
 meta = \case
-  AddR -> Meta (match "add" ./ end) qend (body bodyPerson) (resp bodyKey) M.post
-  EditR -> Meta (match "edit" ./ capture key ./ end) qend (body bodyPerson) (resp bodyUnit) M.put
-  DeleteR -> Meta (match "delete" ./ capture key ./ end) qend bodyless (resp bodyUnit) M.delete
-  ViewR -> Meta (match "view" ./ capture key ./ end) qend bodyless (resp bodyPerson) M.get
-  ViewAllR -> Meta (match "view-all" ./ end) (optional "limit" int .& qend) bodyless (resp bodyKeyed) M.get
+  AddR -> Meta (match "add" ./ end) qend (body $ one bodyPerson) (resp $ one bodyKey) M.post
+  EditR -> Meta (match "edit" ./ capture key ./ end) qend (body $ one bodyPerson) (resp $ one bodyUnit) M.put
+  DeleteR -> Meta (match "delete" ./ capture key ./ end) qend bodyless (resp $ one bodyUnit) M.delete
+  ViewR -> Meta (match "view" ./ capture key ./ end) qend bodyless (resp $ one bodyPerson) M.get
+  ViewAllR -> Meta (match "view-all" ./ end) (optional "limit" int .& qend) bodyless (resp $ one bodyKeyed) M.get
 
 key :: CaptureCodec Key
 key = showReadCaptureCodec
@@ -78,9 +70,7 @@ allRoutes :: [Constructed Route]
 allRoutes = [Constructed AddR, Constructed EditR, Constructed DeleteR, Constructed ViewR, Constructed ViewAllR]
 
 prepare :: Route cs qs rq rp -> Arguments cs qs rq (Prepared Route rp)
-prepare = prepareWith (metaPath . meta) (metaQuery . meta) (metaRequestBody . meta)
+prepare = prepareWith meta
 
 link :: Prepared Route rp -> Url
-link =
-  linkWith (mapPath captureCodecToCaptureEncoding . metaPath . meta)
-           (mapQuery captureCodecToCaptureEncoding . metaQuery . meta)
+link = linkWith (mapMeta captureEncoding captureEncoding id id . meta)
