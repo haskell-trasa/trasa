6137dd7ef34c68968368eca594491d84c5b21cb9
chessai <chessai1996@gmail.com>
2019-03-24 17:43:57 -0400

    update example

--- example/frontend/app/Main.hs
+++ example/frontend/app/Main.hs
@@ -1,12 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
 import Data.FileEmbed (embedFile)
-import Reflex.Dom
+import Common
+import Trasa.Core (metaCodecToMetaClient)
+import Trasa.Error
+import Trasa.Client
+import Network.HTTP.Client
 
-app :: MonadWidget t m => m ()
-app = return ()
+--css :: Text
+--css = $(embedFile "semantic.min.css")
+
+scheme :: Scheme
+scheme = Http
+
+authority :: Authority
+authority = Authority scheme "127.0.0.1" Nothing
+
+config :: IO Config
+config = do
+  mngr <- newManager defaultManagerSettings 
+  pure $ Config authority mempty mngr
+
+client :: IO (Either TrasaErr resp)
+client = do
+  cfg <- config
+  clientWith (metaCodecToMetaClient . meta) cfg _
 
 main :: IO ()
-main = mainWidgetWithCss $(embedFile "semantic.min.css") app
+main = pure ()
